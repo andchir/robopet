67a512b18fdad4bbbd0d9b07d0badf41ae6ef5a0
andchir <andchir@gmail.com>
2026-04-05 18:47:08 +0300

    screenshots update, add translation

--- mobile/src/app/home/home.module.ts
+++ mobile/src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { TranslocoModule } from '@jsverse/transloco';
 import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { RobotFaceComponent } from '../components/robot-face/robot-face.component';
@@ -14,6 +15,7 @@ import { VoiceButtonComponent } from '../components/voice-button/voice-button.co
     FormsModule,
     IonicModule,
     HomePageRoutingModule,
+    TranslocoModule,
   ],
   declarations: [
     HomePage,
